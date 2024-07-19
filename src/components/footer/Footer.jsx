d0934b5701c8a436e34f66f5ce426a30bbbc546c
ALMardasDEV <albusaidi9094@gmail.com>
2024-07-19 19:28:37 +0400

    footer fix2

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 const Footer = () => {
   return (
-    <div className='flex flex-col justify-between  h-[10vh] items-center flex-col lg:flex-row mt-10 border-t-2'>
+    <div className='flex flex-col justify-between  h-[10vh] items-center  lg:flex-row mt-10 border-t-2'>
       <div><Link target="_blank" href='https://www.instagram.com/_5lo_9/'>developed By@AlMardas</Link></div>
       <div><h1>جميع الحقوق محفوظة لـزعتر@٢٠٢٤</h1></div>
       
